7a24ace371abcb988aa7f975a73078db46f7d54a
Hu Wenqi <wenqi.hu@lazada.com>
2022-07-27 12:14:32 +0800

    Add jsp suffix for page search

--- src/main/java/com/wenqi/bootwebdemo/HomeController.java
+++ src/main/java/com/wenqi/bootwebdemo/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @RequestMapping("home")
-    public void home(){
+    public String home(){
         //method name does not matter
-
+        return "home"; //because we set up a suffix search in app prop file
 
     }
 }
