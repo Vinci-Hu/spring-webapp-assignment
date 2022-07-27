9a87eb2c9167201593818056d9d50c8cadf64228
Hu Wenqi <wenqi.hu@lazada.com>
2022-07-27 11:11:11 +0800

    Add logger

--- src/main/java/com/wenqi/bootwebdemo/Person.java
+++ src/main/java/com/wenqi/bootwebdemo/Person.java
@@ -1,9 +1,17 @@
 package com.wenqi.bootwebdemo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Person implements Creature{
+
+    private static Logger logger = LogManager.getLogger(Person.class);
+
+    public Person() {
+        logger.info("A Person is born.");
+    }
 
     @Override
     public void breathe() {
