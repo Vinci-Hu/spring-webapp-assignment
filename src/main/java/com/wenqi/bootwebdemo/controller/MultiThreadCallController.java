0c5aba4480b5ff978131e6f2042513ad02378407
Hu Wenqi <wenqi.hu@lazada.com>
2022-08-10 14:19:28 +0800

    Multi controller now uses CompletableFuture and returns list of response

--- src/main/java/com/wenqi/bootwebdemo/controller/MultiThreadCallController.java
+++ src/main/java/com/wenqi/bootwebdemo/controller/MultiThreadCallController.java
@@ -1,14 +1,15 @@
 package com.wenqi.bootwebdemo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.wenqi.bootwebdemo.model.MultiResponseDTO;
 import com.wenqi.bootwebdemo.model.MultiThreadCallRequestDTO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import static java.util.stream.Collectors.joining;
 
@@ -30,9 +35,11 @@ public class MultiThreadCallController {
     ObjectMapper objectMapper;
 
     @GetMapping(value = "/multi")
-    public String callExternalMultiThread(MultiThreadCallRequestDTO requestDTO) {
+    public List<MultiResponseDTO> callExternalMultiThread(MultiThreadCallRequestDTO requestDTO) {
         Integer threadCnt = requestDTO.getThread_cnt();
+        // Now assert thread = 3
 
+        // Generate url for external get request
         String prefix = "https://grecom.taobao.com/recommend?";
         String suffix = "&language=en-SG&size=15&region_id=ph&need_cross_module_dedup=false";
         Map<String, String> requestParams = new HashMap<>();
@@ -41,7 +48,7 @@ public class MultiThreadCallController {
         requestParams.put("user_id", String.valueOf(requestDTO.getUser_id()));
         requestParams.put("pageSize", String.valueOf(requestDTO.getPageSize()));
 
-        String encodedURL = requestParams.keySet().stream().map(key -> {
+        String encodedUrl = requestParams.keySet().stream().map(key -> {
             try {
                 return key + "=" + encodeValue(requestParams.get(key));
             } catch (UnsupportedEncodingException e) {
@@ -49,29 +56,46 @@ public class MultiThreadCallController {
                 throw new RuntimeException();
             }
         }).collect(joining("&", prefix, suffix));
-        logger.info("External HTTP url produced. url: " + encodedURL);
+        logger.info("Multi call api: External HTTP url produced. url: " + encodedUrl);
 
+        // Create an arraylist for response objects
+        List<MultiResponseDTO> responseDTOList = new ArrayList<>();
+
+        try {
+            responseDTOList = runMultiThread(3, encodedUrl, responseDTOList);
+        } catch (ExecutionException e) {
+            logger.error("Multi call api: future exceution exception detected.");
+        } catch (InterruptedException e) {
+            logger.error("Multi call api: future exceution interrupted.");
+        }
+        return responseDTOList;
+    }
+
+    private List<MultiResponseDTO> runMultiThread(Integer threadCnt,
+                                                   String encodedUrl,
+                                                   List<MultiResponseDTO> responseDTOList) throws ExecutionException, InterruptedException {
         Runnable runnable = () -> {
             try {
                 String name = Thread.currentThread().getName();
-                HttpResponse<JsonNode> response = Unirest.get(encodedURL).asJson();
+                HttpResponse<JsonNode> response = Unirest.get(encodedUrl).asJson();
                 JSONObject responseBody = response.getBody().getObject();
-                String tpp_trace = responseBody.getString("tpp_trace");
-                logger.info("Thread " + name + ": tpp_trace = " + tpp_trace);
+                MultiResponseDTO responseDTO = objectMapper.readValue(responseBody.toString(), MultiResponseDTO.class);
+                responseDTOList.add(responseDTO);
+                logger.info("Thread " + name + ": tpp_trace = " + responseDTO.getTppTrace());
             } catch (UnirestException e) {
                 logger.error("Multi call api: Unirest Get response as json fails.");
+            } catch (JsonProcessingException e) {
+                logger.error("Multi call api: Json processing of response fails.");
             }
         };
 
-//        runnable.run();
-
-        for (int i = 0; i < threadCnt; i++) {
-            Thread thread = new Thread(runnable);
-            thread.start();
-        }
-        return "success";
+        CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(
+                CompletableFuture.runAsync(runnable),
+                CompletableFuture.runAsync(runnable),
+                CompletableFuture.runAsync(runnable));
+        combinedFuture.get();
+        return responseDTOList;
     }
-
     private String encodeValue(String value) throws UnsupportedEncodingException {
         return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
     }
